18eee4d963fc73e32f17d8d35f8bc108ddf5b54a
ultranaveed <naveednovo01@gmail.com>
2025-04-27 15:35:15 +0600

    Category CRUD Done

--- WebAppASP/Models/Category.cs
+++ WebAppASP/Models/Category.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace WebAppASP.Models{
@@ -5,7 +6,11 @@ namespace WebAppASP.Models{
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
     
